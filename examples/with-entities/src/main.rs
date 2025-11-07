7ea6625777f1a507cee4f123384e36328a76d3b3
rozgo <alex.rozgo@gmail.com>
2025-11-06 23:56:38 -0600

    stripe builder, prop ergonomics

--- examples/with-entities/src/main.rs
+++ examples/with-entities/src/main.rs
@@ -14,24 +14,24 @@ fn App() -> impl IntoView {
         <ViewerContainer ion_token=ion_token>
             // Red rectangle entity
             <Entity
-                name=Signal::derive(|| Some("Red Rectangle".to_string()))
-                description=Signal::derive(|| Some("A red semi-transparent rectangle".to_string()))
+                name=Some("Red Rectangle".to_string())
+                description=Some("A red semi-transparent rectangle".to_string())
             >
                 <RectangleGraphics
-                    coordinates=create_js_signal(Rectangle::from_degrees(-110.0, 20.0, -80.0, 25.0))
-                    material=create_js_signal(Some(Material::color(Color::red().with_alpha(0.5))))
-                    outline=Signal::derive(|| Some(true))
-                    outline_color=create_js_signal(Some(Color::black()))
+                    coordinates=Rectangle::from_degrees(-110.0, 20.0, -80.0, 25.0)
+                    material=Some(Material::color(Color::red().with_alpha(0.5)))
+                    outline=Some(true)
+                    outline_color=Some(Color::black())
                 />
             </Entity>
 
             // Blue polygon entity with hole
             <Entity
-                name=Signal::derive(|| Some("Blue Polygon".to_string()))
-                description=Signal::derive(|| Some("A blue polygon with holes".to_string()))
+                name=Some("Blue Polygon".to_string())
+                description=Some("A blue polygon with holes".to_string())
             >
                 <PolygonGraphics
-                    hierarchy=create_js_signal({
+                    hierarchy={
                         let positions = cartesian3_from_degrees_array(&[
                             -115.0, 37.0,
                             -115.0, 32.0,
@@ -40,55 +40,47 @@ fn App() -> impl IntoView {
                             -102.0, 35.0,
                         ]);
                         PolygonHierarchy::new_simple(&positions.into())
-                    })
-                    material=create_js_signal(Some(Material::color(Color::blue().with_alpha(0.5))))
-                    outline=Signal::derive(|| Some(true))
-                    outline_color=create_js_signal(Some(Color::white()))
+                    }
+                    material=Some(Material::color(Color::blue().with_alpha(0.5)))
+                    outline=Some(true)
+                    outline_color=Some(Color::white())
                 />
             </Entity>
 
             // Green ellipse entity
             <Entity
-                name=Signal::derive(|| Some("Green Ellipse".to_string()))
-                description=Signal::derive(|| Some("A green ellipse".to_string()))
-                position=create_js_signal(Some(cartesian3_from_degrees(-95.0, 40.0, 0.0)))
+                name=Some("Green Ellipse".to_string())
+                description=Some("A green ellipse".to_string())
+                position=Some(cartesian3_from_degrees(-95.0, 40.0, 0.0))
             >
                 <EllipseGraphics
-                    semi_minor_axis=Signal::derive(|| 300000.0)
-                    semi_major_axis=Signal::derive(|| 500000.0)
-                    material=create_js_signal(Some(Material::color(Color::green().with_alpha(0.5))))
-                    outline=Signal::derive(|| Some(true))
-                    outline_color=create_js_signal(Some(Color::black()))
-                    rotation=Signal::derive(|| Some(to_radians(45.0)))
+                    semi_minor_axis=300000.0
+                    semi_major_axis=500000.0
+                    material=Some(Material::color(Color::green().with_alpha(0.5)))
+                    outline=Some(true)
+                    outline_color=Some(Color::black())
+                    rotation=Some(to_radians(45.0))
                 />
             </Entity>
 
             // Striped rectangle entity
             <Entity
-                name=Signal::derive(|| Some("Striped Rectangle".to_string()))
-                description=Signal::derive(|| Some("A rectangle with stripe pattern".to_string()))
+                name=Some("Striped Rectangle".to_string())
+                description=Some("A rectangle with stripe pattern".to_string())
             >
                 <RectangleGraphics
-                    coordinates=create_js_signal(Rectangle::from_degrees(-92.0, 30.0, -76.0, 36.0))
-                    material=create_js_signal(Some({
-                        use js_sys::Object;
-                        use wasm_bindgen::JsValue;
-                        let options = Object::new();
-                        let _ = js_sys::Reflect::set(&options, &JsValue::from_str("evenColor"), &JsValue::from(Color::white()));
-                        let _ = js_sys::Reflect::set(&options, &JsValue::from_str("oddColor"), &JsValue::from(Color::blue()));
-                        let _ = js_sys::Reflect::set(&options, &JsValue::from_str("repeat"), &JsValue::from_f64(5.0));
-                        Material::stripe(StripeMaterialProperty::new(&options))
-                    }))
-                    outline=Signal::derive(|| Some(true))
-                    outline_color=create_js_signal(Some(Color::black()))
+                    coordinates=Rectangle::from_degrees(-92.0, 30.0, -76.0, 36.0)
+                    material=Some(Material::stripe(
+                        StripeOptions::new()
+                            .even_color(Color::white())
+                            .odd_color(Color::blue())
+                            .repeat(5.0)
+                            .build()
+                    ))
+                    outline=Some(true)
+                    outline_color=Some(Color::black())
                 />
             </Entity>
         </ViewerContainer>
     }
-}
-
-/// Helper to create JsSignal from a value
-fn create_js_signal<T: 'static>(value: T) -> leptos_cesium::core::JsSignal<T> {
-    use leptos_cesium::core::JsRwSignal;
-    JsRwSignal::new_local(value).read_only().into()
 }
--- leptos-cesium/src/bindings/materials/stripe.rs
+++ leptos-cesium/src/bindings/materials/stripe.rs
@@ -1,5 +1,7 @@
 //! Cesium StripeMaterialProperty
 
+use crate::bindings::Color;
+
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
@@ -12,6 +14,91 @@ extern "C" {
 
     #[wasm_bindgen(constructor, js_namespace = Cesium)]
     pub fn new(options: &JsValue) -> StripeMaterialProperty;
+}
+
+/// Builder for creating StripeMaterialProperty with a fluent API
+#[derive(Default)]
+pub struct StripeOptions {
+    even_color: Option<Color>,
+    odd_color: Option<Color>,
+    repeat: Option<f64>,
+    offset: Option<f64>,
+}
+
+impl StripeOptions {
+    /// Create a new StripeOptions builder
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Set the even stripe color
+    pub fn even_color(mut self, color: Color) -> Self {
+        self.even_color = Some(color);
+        self
+    }
+
+    /// Set the odd stripe color
+    pub fn odd_color(mut self, color: Color) -> Self {
+        self.odd_color = Some(color);
+        self
+    }
+
+    /// Set the number of times the stripes repeat
+    pub fn repeat(mut self, repeat: f64) -> Self {
+        self.repeat = Some(repeat);
+        self
+    }
+
+    /// Set the offset of the stripes
+    pub fn offset(mut self, offset: f64) -> Self {
+        self.offset = Some(offset);
+        self
+    }
+
+    /// Build the StripeMaterialProperty
+    #[cfg(target_arch = "wasm32")]
+    pub fn build(self) -> StripeMaterialProperty {
+        use js_sys::{Object, Reflect};
+
+        let options = Object::new();
+
+        if let Some(color) = self.even_color {
+            let _ = Reflect::set(
+                &options,
+                &JsValue::from_str("evenColor"),
+                &JsValue::from(color),
+            );
+        }
+        if let Some(color) = self.odd_color {
+            let _ = Reflect::set(
+                &options,
+                &JsValue::from_str("oddColor"),
+                &JsValue::from(color),
+            );
+        }
+        if let Some(repeat) = self.repeat {
+            let _ = Reflect::set(
+                &options,
+                &JsValue::from_str("repeat"),
+                &JsValue::from_f64(repeat),
+            );
+        }
+        if let Some(offset) = self.offset {
+            let _ = Reflect::set(
+                &options,
+                &JsValue::from_str("offset"),
+                &JsValue::from_f64(offset),
+            );
+        }
+
+        StripeMaterialProperty::new(&options.into())
+    }
+
+    /// Build the StripeMaterialProperty
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn build(self) -> StripeMaterialProperty {
+        StripeMaterialProperty
+    }
 }
 
 #[cfg(not(target_arch = "wasm32"))]
